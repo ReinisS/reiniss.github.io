dff3776fa67f51fa21b543ffdf53904475786d45
ReinisS <reinissestakovskis@gmail.com>
2020-07-13 21:14:53 +0300

    Changed away from .webp images

--- assets/style-switcher.js
+++ assets/style-switcher.js
@@ -5,7 +5,7 @@ $('.fontawesomebg--light').hide();
 //Change dark_theme.css to color.css
 $('.fontawesomebg--light').click(function (){
     $('link[href="./assets/dark_theme.css"]').attr('href','./assets/color.css');
-    // $('img[src$="./assets/images/img_avatar_monochrome.webp"]').attr("src","./assets/images/img_avatar.webp");
+    // $('img[src$="./assets/images/img_avatar_monochrome.jpg"]').attr("src","./assets/images/img_avatar.jpg");
     $('.fontawesomebg--light').toggle();
     $('.fontawesomebg--dark').toggle();
 });
@@ -13,7 +13,7 @@ $('.fontawesomebg--light').click(function (){
 //Change color.css to dark_theme.css
 $('.fontawesomebg--dark').click(function (){
     $('link[href="./assets/color.css"]').attr('href','./assets/dark_theme.css');
-    // $('img[src$="./assets/images/img_avatar.webp"]').attr("src","./assets/images/img_avatar_monochrome.webp");
+    // $('img[src$="./assets/images/img_avatar.jpg"]').attr("src","./assets/images/img_avatar_monochrome.jpg");
     $('.fontawesomebg--light').toggle();
     $('.fontawesomebg--dark').toggle();
 });
