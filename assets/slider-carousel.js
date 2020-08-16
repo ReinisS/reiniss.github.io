0a9c48130471c1f97f0f4a3f347395f715d8d3e3
ReinisS <reinissestakovskis@gmail.com>
2020-08-16 22:40:02 +0300

    Added projects section, adjusted some text, added dark theme CSS preload

--- assets/slider-carousel.js
+++ assets/slider-carousel.js
@@ -1,15 +1,29 @@
 'use strict';
 
-//Settings for carousel from bootstrap 4.0
-$('.carousel').carousel({
-    pause: "hover",
-    interval: 5000
-});
+var slidesClasses = {
+    "bandItSlides": 1,
+    "pyPriceSlides": 1,
+    "audioUpsamplerSlides": 1,
+};
 
-$(".carousel-control-prev").click(function(){
-    $(".carousel").carousel("prev");
-});
+for (var slidesClass in slidesClasses) {
+    // console.log(slidesClass, slidesClasses[slidesClass]);
+    showDivs(slidesClasses[slidesClass], slidesClass);
+}
 
-$(".carousel-control-next").click(function(){
-    $(".carousel").carousel("next");
-});
+// showDivs(slideIndex, slidesClass);
+
+function plusDivs(n, slidesClass) {
+    showDivs(slidesClasses[slidesClass] += n, slidesClass);
+}
+
+function showDivs(n, slidesClass) {
+    var i;
+    var x = document.getElementsByClassName(slidesClass);
+    if (n > x.length) { slidesClasses[slidesClass] = 1 }
+    if (n < 1) { slidesClasses[slidesClass] = x.length };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slidesClasses[slidesClass] - 1].style.display = "block";
+}
